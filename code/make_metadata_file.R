5ed432bfd0cd4dc85f3d251100b89dab5606b14c
marcsze <marcasze@gmail.com>
2017-11-09 08:35:15 -0500

    add comments

--- code/make_metadata_file.R
+++ code/make_metadata_file.R
@@ -12,6 +12,7 @@ loadLibs(c("tidyverse", "stringr"))
 raw_file <- read_tsv("data/process/amp.files", col_names = F) %>% 
   rename(full_name = X1) %>% select(full_name)
 
+# separate the mock samples into specified categories
 separated_mock_file <- raw_file %>% 
   filter(str_detect(full_name, "DA1") == F) %>% 
   separate(full_name, c("cycles", "taq", "sample_type", "sample_name")) %>% 
@@ -20,16 +21,17 @@ separated_mock_file <- raw_file %>%
   mutate(cycles = str_replace(cycles, "PMM", "30x"), 
          taq = str_replace(taq, "ZymoControl(\\d)", "ACC"))
 
-
+# Separate the fecal samples into specified categories
 separated_samples_file <- raw_file %>% 
   filter(str_detect(full_name, "DA1") == T) %>% 
   separate(full_name, c("cycles", "taq", "ext_kit", "sample_type", "sample_name")) %>% 
   bind_cols(filter(raw_file, str_detect(full_name, "DA1") == T)[, "full_name"]) %>% 
   select(full_name, cycles, taq, sample_type, sample_name)
 
-
+# Combine the two files back together
 combined_file <- separated_mock_file %>% bind_rows(separated_samples_file)
 
+# Write out the combined file as the finalized nice meta data file
 write_csv(combined_file, "data/process/tables/meta_data.csv")
 
 
