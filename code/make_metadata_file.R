398367aa08849ade41569ad4011eff3e5a4439f4
marcsze <marcasze@gmail.com>
2017-11-09 08:33:36 -0500

    complete code for nice metadata file

--- code/make_metadata_file.R
+++ code/make_metadata_file.R
@@ -1,5 +1,5 @@
 ### Make nice master meta data file
-### Create a master file to glue all other files together based on the files file
+### Create a master files based on information in the used files file
 ## Marc Sze
 
 
@@ -14,11 +14,23 @@ raw_file <- read_tsv("data/process/amp.files", col_names = F) %>%
 
 separated_mock_file <- raw_file %>% 
   filter(str_detect(full_name, "DA1") == F) %>% 
-  separate(full_name, c("cycles", "taq", "sample_type", "sample_number")) %>% 
+  separate(full_name, c("cycles", "taq", "sample_type", "sample_name")) %>% 
   bind_cols(filter(raw_file, str_detect(full_name, "DA1") == F)[, "full_name"]) %>% 
-  mutate(sample_number = str_replace(sample_number, "DNA", "")) %>% 
-  select(full_name, cycles, taq, sample_type, sample_number) %>% 
+  select(full_name, cycles, taq, sample_type, sample_name) %>% 
   mutate(cycles = str_replace(cycles, "PMM", "30x"), 
          taq = str_replace(taq, "ZymoControl(\\d)", "ACC"))
+
+
+separated_samples_file <- raw_file %>% 
+  filter(str_detect(full_name, "DA1") == T) %>% 
+  separate(full_name, c("cycles", "taq", "ext_kit", "sample_type", "sample_name")) %>% 
+  bind_cols(filter(raw_file, str_detect(full_name, "DA1") == T)[, "full_name"]) %>% 
+  select(full_name, cycles, taq, sample_type, sample_name)
+
+
+combined_file <- separated_mock_file %>% bind_rows(separated_samples_file)
+
+write_csv(combined_file, "data/process/tables/meta_data.csv")
+
 
 
