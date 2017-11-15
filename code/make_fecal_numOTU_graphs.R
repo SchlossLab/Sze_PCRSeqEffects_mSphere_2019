7a01d0fc8d6cd990c19ce0a19959050505cc84a7
marcsze <marcasze@gmail.com>
2017-11-15 15:31:02 -0500

    add extra sub sampling to code for graph

--- code/make_fecal_numOTU_graphs.R
+++ code/make_fecal_numOTU_graphs.R
@@ -31,7 +31,7 @@ read_data <- function(pathing, start_name, end_name, differentiator){
 ###########################################################################################################################
 
 # Vector of sub samples used
-sub_sample_level <- c("1000", "5000", "10000")
+sub_sample_level <- c("1000", "5000", "10000", "15000", "20000")
 
 # Read in the count data
 numOTU_data <- sapply(sub_sample_level, 
@@ -49,7 +49,7 @@ thousand_graph <- numOTU_data[["1000"]] %>%
   scale_color_manual(name = "Taq Used", 
                      values = c("#440154FF", "#3B528BFF", "#21908CFF", "#5DC863FF", "#FDE725FF")) + 
   labs(x = "Amplification Cycles", y = expression(Log["2"]~Number~of~OTUs)) + 
-  ggtitle("A") + coord_cartesian(ylim = c(0, 9)) + 
+  ggtitle("A") + coord_cartesian(ylim = c(0, 9.2)) + 
   theme(plot.title = element_text(face="bold", hjust = -0.07, size = 20), 
         panel.grid.major = element_blank(), 
         panel.grid.minor = element_blank(), 
@@ -70,7 +70,7 @@ five_thousand_graph <- numOTU_data[["5000"]] %>%
   scale_color_manual(name = "Taq Used", 
                      values = c("#440154FF", "#3B528BFF", "#21908CFF", "#5DC863FF", "#FDE725FF")) + 
   labs(x = "Amplification Cycles", y = expression(Log["2"]~Number~of~OTUs)) + 
-  ggtitle("B") + coord_cartesian(ylim = c(0, 9)) + 
+  ggtitle("A") + coord_cartesian(ylim = c(0, 9.2)) + 
   theme(plot.title = element_text(face="bold", hjust = -0.07, size = 20), 
         panel.grid.major = element_blank(), 
         panel.grid.minor = element_blank(), 
@@ -86,12 +86,12 @@ ten_thousand_graph <- numOTU_data[["10000"]] %>%
                       levels = c("ACC", "K", "PHU", "PL", "Q5"), 
                       labels = c("Accuprime", "Kappa", "Phusion", "Platinum", "Q5"))) %>% 
   ggplot(aes(cycles, log2(numOTUs), color = taq, group = taq)) + 
-  geom_point(size = 2, alpha = 0.7) + theme_bw() + 
+  geom_point(size = 2, alpha = 0.7, show.legend = F) + theme_bw() + 
   facet_grid(. ~ sample_name) + 
   scale_color_manual(name = "Taq Used", 
                      values = c("#440154FF", "#3B528BFF", "#21908CFF", "#5DC863FF", "#FDE725FF")) + 
   labs(x = "Amplification Cycles", y = expression(Log["2"]~Number~of~OTUs)) + 
-  ggtitle("C") + coord_cartesian(ylim = c(0, 9)) + 
+  ggtitle("B") + coord_cartesian(ylim = c(0, 9.2)) + 
   theme(plot.title = element_text(face="bold", hjust = -0.07, size = 20), 
         panel.grid.major = element_blank(), 
         panel.grid.minor = element_blank(), 
@@ -102,6 +102,48 @@ ten_thousand_graph <- numOTU_data[["10000"]] %>%
         legend.background = element_rect(color = "black"))
 
 
-combined_graph <- grid.arrange(thousand_graph, five_thousand_graph, ten_thousand_graph, nrow = 3)
+fifteen_thousand_graph <- numOTU_data[["15000"]] %>% 
+  mutate(taq = factor(taq, 
+                      levels = c("ACC", "K", "PHU", "PL", "Q5"), 
+                      labels = c("Accuprime", "Kappa", "Phusion", "Platinum", "Q5"))) %>% 
+  ggplot(aes(cycles, log2(numOTUs), color = taq, group = taq)) + 
+  geom_point(size = 2, alpha = 0.7, show.legend = T) + theme_bw() + 
+  facet_grid(. ~ sample_name) + 
+  scale_color_manual(name = "Taq Used", 
+                     values = c("#440154FF", "#3B528BFF", "#21908CFF", "#5DC863FF", "#FDE725FF")) + 
+  labs(x = "Amplification Cycles", y = expression(Log["2"]~Number~of~OTUs)) + 
+  ggtitle("C") + coord_cartesian(ylim = c(0, 9.2)) + 
+  theme(plot.title = element_text(face="bold", hjust = -0.07, size = 20), 
+        panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank(), 
+        axis.text.y = element_text(size = 10), 
+        legend.position = c(0.08, 0.3), 
+        legend.title = element_blank(), 
+        legend.key = element_blank(), 
+        legend.background = element_rect(color = "black"))
+
+
+twenty_thousand_graph <- numOTU_data[["20000"]] %>% 
+  mutate(taq = factor(taq, 
+                      levels = c("ACC", "K", "PHU", "PL", "Q5"), 
+                      labels = c("Accuprime", "Kappa", "Phusion", "Platinum", "Q5"))) %>% 
+  ggplot(aes(cycles, log2(numOTUs), color = taq, group = taq)) + 
+  geom_point(size = 2, alpha = 0.7) + theme_bw() + 
+  facet_grid(. ~ sample_name) + 
+  scale_color_manual(name = "Taq Used", 
+                     values = c("#440154FF", "#3B528BFF", "#21908CFF", "#5DC863FF", "#FDE725FF")) + 
+  labs(x = "Amplification Cycles", y = expression(Log["2"]~Number~of~OTUs)) + 
+  ggtitle("E") + coord_cartesian(ylim = c(0, 9.5)) + 
+  theme(plot.title = element_text(face="bold", hjust = -0.07, size = 20), 
+        panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank(), 
+        axis.text.y = element_text(size = 10), 
+        legend.position = c(0.08, 0.3), 
+        legend.title = element_blank(), 
+        legend.key = element_blank(), 
+        legend.background = element_rect(color = "black"))
+
+
+combined_graph <- grid.arrange(five_thousand_graph, ten_thousand_graph, fifteen_thousand_graph, nrow = 3)
 
 ggsave("results/figures/fecal_numOTU_graph.pdf", combined_graph, width = 8, height = 11, dpi = 300)
