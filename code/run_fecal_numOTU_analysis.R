7ebff79940de7faa2860c8ab0854673f7ef6196c
marcsze <marcasze@gmail.com>
2017-11-16 07:33:11 -0500

    first commit

---
+++ code/run_fecal_numOTU_analysis.R
@@ -0,0 +1,8 @@
+### Create graphs of the different numOTUs based on cycle number and sub sampling
+### Fecal Sample Community only
+### Marc Sze
+
+# Load in needed functions and libraries
+source('code/functions.R')
+
+loadLibs(c("tidyverse", "stringr", "viridis", "gridExtra"))
