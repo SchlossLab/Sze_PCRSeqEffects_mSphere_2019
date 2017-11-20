1ed78ee3b018dceaa35858bc776c01e3778bc9a4
marcsze <marcasze@gmail.com>
2017-11-20 07:35:52 -0500

    add data read in

--- code/run_seq_error_table_creations.R
+++ code/run_seq_error_table_creations.R
@@ -8,4 +8,44 @@ source('code/functions.R')
 loadLibs(c("tidyverse", "stringr"))
 
 
+###########################################################################################################################
+############################### List of functions to make analysis work ###################################################
+###########################################################################################################################
+
+# Function to read in the data
+read_data <- function(pathing, start_name, end_name, differentiator){
+  # pathing is the variable that controls the path
+  # start_name is the beginning of the file
+  # end_name is how the file ends (e.g. .csv)
+  # differentiator is the unique component of the file name
+  
+  # reads in the respective file 
+  tempData <- read_tsv(paste(pathing, start_name, differentiator, end_name, sep = ""), na = character())
+  # returns the file to the global environment
+  return(tempData)
+  
+}
+
+
+
+
+
+
+
+###########################################################################################################################
+############################### Run actual analysis programs  #############################################################
+###########################################################################################################################
+
+# Create vector with different subsample levels
+sub_sample_level <- c("50", "100", "500", "1000", "5000", "10000")
+
+# Read in needed count files
+count_tables <- sapply(sub_sample_level, 
+                          function(x) read_data("data/process/", "mock_error.", ".count_table", x), 
+                          simplify = F)
+
+test <- read_tsv("data/process/mock_error.1000.summary", na = character())
+
+# Read in seq data
+
 
