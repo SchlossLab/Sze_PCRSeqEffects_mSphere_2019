a64d0633dfb6145b263934aaa8aa1b254adda0a8
marcsze <marcasze@gmail.com>
2017-11-20 07:16:03 -0500

    first commit

---
+++ code/run_seq_error_table_creations.R
@@ -0,0 +1,11 @@
+### Code to read in and process seq error files
+### transform to make it easier for R analysis
+### Marc Sze
+
+# Load in needed functions and libraries
+source('code/functions.R')
+
+loadLibs(c("tidyverse", "stringr"))
+
+
+
