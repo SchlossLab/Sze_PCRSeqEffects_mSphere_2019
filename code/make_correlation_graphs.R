9869ab6950769345936eb0166e60491782bcb0e7
marcsze <marcasze@gmail.com>
2017-12-08 08:51:35 -0500

    first commit

---
+++ code/make_correlation_graphs.R
@@ -0,0 +1,50 @@
+### Correlations between error and chimera with number of OTUs
+### Mock Community only
+### Marc Sze
+
+# Load in needed functions and libraries
+source('code/functions.R')
+
+loadLibs(c("tidyverse", "stringr", "dunn.test"))
+
+
+###########################################################################################################################
+############################### List of functions to make analysis work ###################################################
+###########################################################################################################################
+
+
+
+# Function to read in the data
+read_data <- function(pathing, start_name, end_name, differentiator){
+  # pathing is the variable that controls the path
+  # start_name is the beginning of the file
+  # end_name is how the file ends (e.g. .csv)
+  # differentiator is the unique component of the file name
+  
+  # reads in the respective file 
+  tempData <- read_csv(paste(pathing, start_name, differentiator, end_name, sep = ""))
+  # returns the file to the global environment
+  return(tempData)
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+###########################################################################################################################
+############################### Run actual analysis programs  #############################################################
+###########################################################################################################################
+
+
+
+
+
