f46cf7b22ebb0025eaa199618300bd2c5c9865b4
marcsze <marcasze@gmail.com>
2017-12-15 15:55:03 -0500

    first commit

---
+++ code/make_bray_distance_graphs.R
@@ -0,0 +1,50 @@
+### Create graphs of the distances compared in 5 cycle increments (Bray)
+### Mock Community only
+### Marc Sze
+
+# Load in needed functions and libraries
+source('code/functions.R')
+
+loadLibs(c("tidyverse", "stringr", "viridis", "gridExtra"))
+
+###########################################################################################################################
+############################### List of functions to make analysis work ###################################################
+###########################################################################################################################
+
+# Function to read in the data
+read_data <- function(pathing, start_name, end_name, differentiator){
+  # pathing is the variable that controls the path
+  # start_name is the beginning of the file
+  # end_name is how the file ends (e.g. .csv)
+  # differentiator is the unique component of the file name
+  
+  # reads in the respective file 
+  tempData <- read_csv(paste(pathing, start_name, differentiator, end_name, sep = ""))
+  # returns the file to the global environment
+  return(tempData)
+  
+}
+
+
+###########################################################################################################################
+############################### Run actual analysis programs  #############################################################
+###########################################################################################################################
+
+# Vector of sub samples used
+sub_sample_level <- c("1000", "5000", "10000", "15000", "20000")
+
+dist_data <- sapply(sub_sample_level, 
+                    function(x) read_csv(paste("data/process/tables/bray_5_cycle_dist_", 
+                                               x, "_data.csv", sep = "")), simplify = F)
+
+
+
+
+
+
+
+
+
+
+
+
