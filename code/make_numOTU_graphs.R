9c67c88820a122df1d9bac2acd0040282b099c85
marcsze <marcasze@gmail.com>
2018-05-14 13:23:11 -0400

    update to remove rarefaction info

--- code/make_numOTU_graphs.R
+++ code/make_numOTU_graphs.R
@@ -45,82 +45,42 @@ numOTU_data <- sapply(sub_sample_level,
   mutate(taq = factor(taq, 
                       levels = c("ACC", "K", "PHU", "PL", "Q5"), 
                       labels = c("Accuprime", "Kappa", "Phusion", "Platinum", "Q5")))
+
+
+summary_data <- numOTU_data %>% 
+  group_by(depth_level, taq, cycles) %>% 
+  summarise(taq_mean = median(numOTUs, na.rm = T), 
+            taq_min = min(numOTUs, na.rm = T), 
+            taq_max = max(numOTUs, na.rm = T))
   
 
 ref_otu_num <- read_tsv("data/process/zymo_mock_only.shared") %>% 
   select(numOtus)
 
 # Generate graph of Mock DNA samples 
-thousand_graph <- numOTU_data %>% filter(depth_level == 1000) %>% 
-  ggplot(aes(cycles, log2(numOTUs), color = taq, group = taq)) + 
-  geom_smooth(size = 1, method = "lm", formula = y ~ poly(x, 2), se = FALSE, show.legend = F) + 
-  geom_point(size = 2, alpha = 0.7, show.legend = F) + 
+thousand_graph <- summary_data %>% filter(depth_level == 1000) %>% 
+  ggplot(aes(cycles, taq_mean, color = taq, group = taq)) + 
+  geom_pointrange(aes(ymin = taq_min, ymax = taq_max), size = 0.4, alpha = 0.9, show.legend = T, position = position_dodge(width = 0.8)) +
+  geom_vline(xintercept=seq(1.5, length(unique(summary_data$depth_level))-0.5, 1), 
+             lwd=1, colour="gray") + 
   geom_hline(yintercept = log2(ref_otu_num$numOtus), linetype = "dashed", size = 1) + 
   theme_bw() + 
-  scale_color_manual(name = "Taq Used", 
+  scale_color_manual(name = "HiFi DNA Polymerase", 
                      values = c("#440154FF", "#3B528BFF", "#21908CFF", "#5DC863FF", "#FDE725FF")) + 
-  labs(x = "Amplification Cycles", y = expression(Log["2"]~Number~of~OTUs)) + 
-  ggtitle("A") + coord_cartesian(ylim = c(2, 8)) + 
-  annotate("text", label = paste("Sub-sampled to 1000 Sequences"), x = 2.5, y = 8, size = 3.5) + 
+  labs(x = "Number of Cycles", y = "Number of OTUs") + 
+  coord_cartesian(ylim = c(0, 70)) + 
+  annotate("text", label = paste("Mock Data"), x = 0.8, y = 70, size = 3.5) + 
   theme(plot.title = element_text(face="bold", hjust = -0.07, size = 20), 
         panel.grid.major = element_blank(), 
         panel.grid.minor = element_blank(), 
         axis.text.y = element_text(size = 10), 
-        legend.position = c(0.20, 0.18), 
+        legend.position = "bottom", 
         legend.title = element_blank(), 
         legend.key = element_blank(), 
         legend.background = element_rect(color = "black"))
 
 
-five_thousand_graph <- numOTU_data %>% filter(depth_level == 5000) %>% 
-  ggplot(aes(cycles, log2(numOTUs), color = taq, group = taq)) + 
-  geom_smooth(size = 1, method = "lm", formula = y ~ poly(x, 2), se = FALSE) + 
-  geom_point(size = 2, alpha = 0.7) + 
-  geom_hline(yintercept = log2(ref_otu_num$numOtus), linetype = "dashed", size = 1) + 
-  theme_bw() + 
-  scale_color_manual(name = "Taq Used", 
-                     values = c("#440154FF", "#3B528BFF", "#21908CFF", "#5DC863FF", "#FDE725FF"), 
-                     limits = c("Accuprime", "Kappa", "Phusion", "Platinum", "Q5"), 
-                     guide = guide_legend(nrow = 2, ncol = 3)) +  
-  labs(x = "Amplification Cycles", y = expression(Log["2"]~Number~of~OTUs)) + 
-  ggtitle("B") + coord_cartesian(ylim = c(2, 8)) + 
-  annotate("text", label = paste("Sub-sampled to 5000 Sequences"), x = 2.5, y = 8, size = 3.5) + 
-  theme(plot.title = element_text(face="bold", hjust = -0.07, size = 20), 
-        panel.grid.major = element_blank(), 
-        panel.grid.minor = element_blank(), 
-        axis.text.y = element_text(size = 10), 
-        legend.position = c(0.5, 0.1), 
-        legend.title = element_blank(), 
-        legend.key = element_blank(), 
-        legend.background = element_rect(color = "black"))
-
-
-
-ten_thousand_graph <- numOTU_data %>% filter(depth_level == 10000) %>%
-  ggplot(aes(cycles, log2(numOTUs), color = taq, group = taq)) + 
-  geom_smooth(size = 1, method = "lm", formula = y ~ poly(x, 2), se = FALSE, show.legend = F) + 
-  geom_point(size = 2, alpha = 0.7, show.legend = F) + 
-  geom_hline(yintercept = log2(ref_otu_num$numOtus), linetype = "dashed", size = 1) + 
-  theme_bw() + 
-  scale_color_manual(name = "Taq Used", 
-                     values = c("#440154FF", "#3B528BFF", "#21908CFF", "#5DC863FF", "#FDE725FF"), 
-                     limits = c("Accuprime", "Kappa", "Phusion", "Platinum", "Q5")) +  
-  labs(x = "Amplification Cycles", y = expression(Log["2"]~Number~of~OTUs)) + 
-  ggtitle("C") + coord_cartesian(ylim = c(2, 8)) + 
-  annotate("text", label = paste("Sub-sampled to 10000 Sequences"), x = 2.5, y = 8, size = 3.5) + 
-  theme(plot.title = element_text(face="bold", hjust = -0.07, size = 20), 
-        panel.grid.major = element_blank(), 
-        panel.grid.minor = element_blank(), 
-        axis.text.y = element_text(size = 10), 
-        legend.position = c(0.2, 0.18), 
-        legend.title = element_blank(), 
-        legend.key = element_blank(), 
-        legend.background = element_rect(color = "black"))
-
-
-combined_graph <- grid.arrange(thousand_graph, five_thousand_graph, ten_thousand_graph, ncol = 3)
-
-ggsave("results/figures/Figure2.pdf", combined_graph, width = 11, height = 7, dpi = 300)
+ggsave("results/figures/Figure2.pdf", thousand_graph, width = 6, height = 5, dpi = 300)
 
 
 
