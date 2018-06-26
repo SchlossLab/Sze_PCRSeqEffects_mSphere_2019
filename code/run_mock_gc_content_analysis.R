09901eed02b9bd071a41f376de2e93110039cb67
marcsze <marcasze@gmail.com>
2018-06-26 14:17:17 -0400

    update to use rarefied count data

--- code/run_mock_gc_content_analysis.R
+++ code/run_mock_gc_content_analysis.R
@@ -13,30 +13,12 @@ loadLibs(c("tidyverse", "stringr", "viridis", "gridExtra", "caret", "dunn.test")
 
 error_summary <- read_tsv("data/process/mock_error.summary")
 
-error_table <- read_tsv("data/process/mock_error.count_table") %>% 
-  select(-contains("30x_ACC_Mock")) %>% 
-  gather(sample_id, abund, -Representative_Sequence, -total) %>% 
-  separate(sample_id, c("x1", "x2", "x3", "x4", "x5")) %>% 
-  filter(is.na(x5) | x3 == "Water") %>% 
-  mutate(
-    x4 = case_when(
-      x2 == "A" ~ "DNA1", 
-      x2 == "B" ~ "DNA2", 
-      x2 == "C" ~ "DNA3", 
-      x2 == "D" ~ "DNA4", 
-      TRUE ~ x4), 
-    x2 = case_when(
-      x1 == "Zmock" ~ "ACC", 
-      TRUE ~ x2),
-    x3 = case_when(
-      x1 == "Zmock" ~ "Mock", 
-      TRUE ~ x3), 
-    x1 = case_when(
-      x1 == "Zmock" ~ "30x", 
-      TRUE ~ x1)) %>% 
-  rename(cycles = x1, taq = x2, sample_type = x3, sample_number = x4) %>% 
-  select(-x5) %>% 
-  filter(sample_type != "Water") %>% 
+error_table <- read_csv("data/process/tables/non_summarised_nucleotide_mock_error_summary.csv") %>% 
+  filter(str_detect(full_name, "30x_ACC_Mock") != T) %>%
+  group_by(taq, cycles, sample_name.y, query) %>%
+  summarise(total_seqs = length(query)) %>% 
+  ungroup() %>% 
+  filter(sample_name.y != "Water") %>% 
   mutate(cycles = case_when(
     cycles == "15x" ~ "fifteen", 
     cycles == "20x" ~ "twenty", 
@@ -63,15 +45,15 @@ summarized_table <- error_summary %>%
 
 # Summarise the counts from each sequence by polymerase and cycle
 seq_counts_table <- error_table %>% 
-  group_by(Representative_Sequence, taq, cycles) %>% 
-  summarise(median_abund = median(abund), 
-            iqr25 = quantile(abund)["25%"],
-            iqr75 = quantile(abund)["75%"],
-            min_abund = min(abund),
-            max_abund = max(abund))
+  group_by(query, taq, cycles) %>% 
+  summarise(median_abund = median(total_seqs), 
+            iqr25 = quantile(total_seqs)["25%"],
+            iqr75 = quantile(total_seqs)["75%"],
+            min_abund = min(total_seqs),
+            max_abund = max(total_seqs))
   
 test <- seq_counts_table %>% 
-  inner_join(select(summarized_table, query, reference, higher_gc), by = c("Representative_Sequence" = "query")) %>% 
+  inner_join(select(summarized_table, query, reference, higher_gc), by = "query") %>% 
   group_by(higher_gc, cycles, taq) %>% 
   summarise(median_of_median_abund = median(median_abund), 
             iqr25 = quantile(median_abund)["25%"],
