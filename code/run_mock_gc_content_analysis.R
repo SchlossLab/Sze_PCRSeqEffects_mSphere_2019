cf713e2a86209e60c68097a05bd85d4643e617ae
marcsze <marcasze@gmail.com>
2018-06-27 07:43:07 -0400

    add whole genome component

--- code/run_mock_gc_content_analysis.R
+++ code/run_mock_gc_content_analysis.R
@@ -29,18 +29,38 @@ error_table <- read_csv("data/process/tables/non_summarised_nucleotide_mock_erro
 
 
 # Reduce down the number of columns and take only the most abundant from each taxa
+
+# Staph aureus GC content 32.70% (PMID 22815443)
+# Listeria monocytogenes GC content 38.04% (PMID 22689239)
+# Escherichia coli GC content 50.6% (NCBI database)
+# Salmonella enterica GC content 52.1% (PMID 22461552)
+# Pseudomonas aeruginosa GC content 66.6% (PMID 17038190)
+# Enterococcus faecalis GC content 38% (PMID 22366422)
+# Lactobacillus fermentum GC content 49.7% (PMID 24072868)
+# Bacillus subtilis GC content 43.61% (PMID 23105055)
+
+wg_median_cutoff <- median(c(32.7, 38.04, 50.6, 52.1, 66.6, 38, 49.7, 43.61))
+
+
 summarized_table <- error_summary %>% 
   group_by(reference) %>% 
   filter(weight == max(weight)) %>% 
   ungroup() %>% 
   select(query, reference, AA, TT, CC, GG, total) %>% 
-  mutate(at_percent = (AA + TT)/total * 100, 
+  mutate(
+    whole_genome_gc_percent = c(32.7, 38.04, 50.6, 52.1, 66.6, 38, 49.7, 43.61), 
+    at_percent = (AA + TT)/total * 100, 
          gc_percent = (CC + GG)/total * 100, 
          subs_percent = 100 - (at_percent + gc_percent), 
          higher_gc = case_when(
            gc_percent > median(gc_percent) ~ "yes", 
            gc_percent <= median(gc_percent) ~ "no", 
+           TRUE ~ "uh oh"), 
+         whole_genome_higher_gc = case_when(
+           whole_genome_gc_percent > wg_median_cutoff ~ "yes", 
+           whole_genome_gc_percent <= wg_median_cutoff ~ "no", 
            TRUE ~ "uh oh"))
+
 
 
 # Summarise the counts from each sequence by polymerase and cycle
@@ -61,17 +81,19 @@ test <- seq_counts_table %>%
             min_abund = min(median_abund),
             max_abund = max(median_abund))
 
+whole_test <- seq_counts_table %>% 
+  inner_join(select(summarized_table, query, reference, whole_genome_higher_gc), by = "query") %>% 
+  group_by(whole_genome_higher_gc, cycles, taq) %>% 
+  summarise(median_of_median_abund = median(median_abund), 
+            iqr25 = quantile(median_abund)["25%"],
+            iqr75 = quantile(median_abund)["75%"],
+            min_abund = min(median_abund),
+            max_abund = max(median_abund))
+
 # Write out the table 
 write_csv(test, "data/process/tables/gc_content_amp_summary.csv")
 
-# Staph aureus GC content 32.70% (PMID 22815443)
-# Listeria monocytogenes GC content 38.04% (PMID 22689239)
-# Escherichia coli GC content 
-# Salmonella enterica GC content 52.1% (PMID 22461552)
-# Pseudomonas aeruginosa GC content 66.6% (PMID 17038190)
-# Enterococcus faecalis GC content 38% (PMID 22366422)
-# Lactobacillus fermentum GC content 49.7% (PMID 24072868)
-# Bacillus subtilis GC content 43.61% (PMID 23105055)
+write_csv(whole_test, "data/process/tables/gc_content_whole_genome_amp_summary.csv")
 
 
 
