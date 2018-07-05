319d9e9d82c1f6bc88c00b8db60232034073cbb5
marcsze <marcasze@gmail.com>
2018-07-05 12:56:50 -0400

    change first figure

--- code/make_gc_content_graph.R
+++ code/make_gc_content_graph.R
@@ -1,4 +1,4 @@
-### Plot RF results MDA for classifying samples
+### Plot GC content graph
 # Marc Sze
 
 
@@ -9,32 +9,41 @@ source("code/functions.R")
 loadLibs(c("tidyverse", "gridExtra", "scales", "viridis", "RColorBrewer", "ggrepel"))
 
 # Load in needed data
-summary_data <- read_csv("data/process/tables/gc_content_amp_summary.csv") %>% 
-  filter(cycles != "fifteen")
+summary_data <- read_csv("data/process/tables/gc_summary_table.csv")
 
+# summary_data <- read_csv("data/process/tables/gc_content_amp_summary.csv") %>% 
+#   filter(cycles != "fifteen")
 
 gc_graph <- summary_data %>% 
   mutate(cycles = factor(cycles, 
                          levels = c("twenty", "twenty_five", "thirty", "thirty_five"), 
                          labels = c("20x", "25x", "30x", "35x")), 
-         higher_gc = factor(higher_gc, 
-                            levels = c("yes", "no"), 
-                            labels = c("Yes", "No")), 
          taq = factor(taq, 
                       levels = c("ACC", "K", "PHU", "PL", "Q5"), 
-                      labels = c("Accuprime", "Kappa", "Phusion", "Platinum", "Q5"))) %>% 
-  ggplot(aes(cycles, log10(median_of_median_abund+1), color = higher_gc)) + 
-  geom_pointrange(aes(ymin = log10(min_abund+1), ymax = log10(max_abund+1)), position = position_dodge(width = 0.7)) + 
-  facet_wrap(~taq) + theme_bw() + 
-  labs(x = "", y = expression(Log["10"]~median~counts)) + 
-  scale_color_manual(name = "Higher 16S V4\nGC Content", values = c('#FF8000', '#8B5A2B')) + 
+                      labels = c("Accuprime", "Kappa", "Phusion", "Platinum", "Q5")), 
+         reference = factor(reference, 
+                            levels = c("Staphylococcus_aureus_16S", "Listeria_monocytogenes_16S", 
+                                       "Enterococcus_faecalis_16S", "Bacillus_subtilis_16S", 
+                                       "Lactobacillus_fermentum_16S", "Escherichia_coli_16S_1", 
+                                       "Salmonella_enterica_16S_1", "Pseudomonas_aeruginosa_16S"), 
+                            labels = c("Staphylococcus aureus\n(GC = 32.7%)", "Listeria monocytogenes\n(GC = 38.0%)", 
+                                       "Enterococcus faecalis\n(GC = 38.0%)", "Bacillus subtilis\n(GC = 43.6%)", 
+                                       "Lactobacillus fermentum\n(GC = 49.7%)", "Escherichia coli\n(GC = 50.6%)", 
+                                       "Salmonella enterica\n(GC = 52.1%)", "Pseudomonas aeruginosa\n(GC = 66.6%)"))) %>% 
+  ggplot(aes(cycles, median_abund, color = reference, group = reference)) + 
+  geom_hline(yintercept = 0.12, linetype = "dashed") + 
+  geom_pointrange(aes(ymin = min_abund, ymax = max_abund)) + 
+  geom_line() + theme_bw() + coord_cartesian(ylim = c(0, 0.3)) + 
+  labs(x = "", y = "Relative Abundance") + 
+  facet_wrap(~taq) + 
+  scale_color_brewer(name = "Bacterial Species", type = "div", palette = "RdBu") + 
   theme(plot.title = element_text(face="bold", hjust = -0.07, size = 20), 
         legend.position = c(0.85, 0.3), 
         legend.text = element_text(size = 8), 
         panel.grid.major = element_blank(), 
         panel.grid.minor = element_blank())
 
-ggsave("results/figures/Figure1.pdf", gc_graph, width = 6, height = 4.5, dpi = 300)
+ggsave("results/figures/Figure1.pdf", gc_graph, width = 6, height = 8, dpi = 300)
 
 
 
